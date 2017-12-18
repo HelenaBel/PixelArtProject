84eecc0f5867d77ace2c9b2a5b2c65e12dfd2474
Elena <HelenaBel@users.noreply.github.com>
2017-12-18 12:07:57 +0000

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,38 +1,35 @@
 // Select color input
-var selectedCol = $('#colorPicker').val();
+var selectedCol = null;
 // Select size input
-var hSize = $('#input_height').val();
-var vSize = $('#input_height').val();
+var hSize = null;
+var vSize = null;
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid(row, col) {
   $('#pixel_canvas').children().remove();
-  for(var r = 0; r < row; r++)
-    { var $curRow = $('<tr></tr>');
-      $('#pixel_canvas').append($curRow);
-      for(var c = 0; c < col; c++)
-          $curRow.append('<td></td>');
-    }
-}
-//event listener for submit
-$( '#sizePicker' ).on( 'click', 'input', function(event) {
-    if(event.target.type === 'submit')
-      {hSize = $('#input_height').val();
-       vSize = $('#input_width').val();
-       event.preventDefault();
-       makeGrid(hSize, vSize);
+  for (var r = 0; r < row; r++){
+       var $curRow = $('<tr></tr>');
+       $('#pixel_canvas').append($curRow);
+       for(var c = 0; c < col; c++) $curRow.append('<td></td>');
       }
+}
+
+//event listener for submit
+$('#sizePicker').on('submit', function(event) {
+  hSize = $('#input_height').val();
+  vSize = $('#input_width').val();
+  event.preventDefault();
+  makeGrid(hSize, vSize);
 });
+
 //event listener for table
-$( '#pixel_canvas' ).on( 'click', 'td', function(event) {
+$('#pixel_canvas').on('click', 'td', function(event) {
   var currCol = this.style.backgroundColor;
   var selColRGB = 'rgb(';
   selectedCol = $('#colorPicker').val();
   selColRGB += parseInt(selectedCol.substr(1,2),16) + ', ' +
                parseInt(selectedCol.substr(3,2),16) + ', ' +
                parseInt(selectedCol.substr(5,2),16) + ')';
-  if(currCol === selColRGB)
-     $(this).css('backgroundColor','');
-  else
-     $(this).css('backgroundColor',selectedCol);
+  if(currCol === selColRGB) $(this).css('backgroundColor','');
+  else $(this).css('backgroundColor',selectedCol);
 });
